7fe4911cbac600e8a2d84c0aeb814d309d214ada
ez-li <eileenli.sf@gmail.com>
2019-07-07 09:23:28 -0700

    finished exercises

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,37 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var elements = [];
+  var root = document.body; // set the first node
+
+  // recursive function
+  var nodeHasClass = function(node) {
+  	// check if current node has class
+  	if (node.classList && node.classList.contains(className)) {
+  		elements.push(node);
+  	}
+  	// check if node has children, call recursion on elements
+  	if (node.hasChildNodes) {
+  		for (var i = 0; i < node.childNodes.length; i++) {
+  			nodeHasClass(node.childNodes[i]);
+  		}
+  	}
+  }
+  // call function on root node
+  nodeHasClass(root);
+
+
+  // if (document.body.contains(className)) {
+  // }
+
+  // for (var i = 0; i < document.body.childNodes.length; i++) {
+  // 	if (document.body.childNodes[i].contains(className)) {
+  // 	}
+  // }
+
+  // if (document.body.classList.contains(className)) {
+  // }
+
+  return elements;
+
 };
